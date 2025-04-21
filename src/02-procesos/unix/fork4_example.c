ec72d4e7261ffd770725b9b48a40ebf87e5191af
JEKinter <javierekinter@gmail.com>
2025-04-20 21:53:12 -0300

    fork4_example added

---
+++ src/02-procesos/unix/fork4_example.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+int main() {
+    pid_t pid;
+    int status;
+
+    printf("Parent process (PID: %d) starting...\n", getpid());
+
+    for (int i = 0; i < 3; i++) {
+        pid = fork();
+        
+        if (pid < 0) {
+            fprintf(stderr, "Fork failed\n");
+            exit(1);
+        } else if (pid == 0) {
+            printf("Child process %d (PID: %d, Parent PID: %d) starting...\n", i + 1, getpid(), getppid());
+            sleep(2);
+            pid_t grandchild_pid = fork();
+
+            if (grandchild_pid < 0) {
+                perror("Fork failed (grandchild)");
+                exit(1);
+            } else if (grandchild_pid == 0) {
+                printf("Grandchild process (PID: %d, Parent PID: %d) starting...\n", getpid(), getppid());
+                sleep(2);
+                exit(0);
+            } else {
+                grandchild_pid = wait(&status);
+                printf("Child process (PID: %d) finished with status %d\n", grandchild_pid, WEXITSTATUS(status));
+                exit(0);
+            }
+        }
+    }
+
+    // Parent waits for all children
+    while ((pid = wait(&status)) > 0) {
+        printf("Child process (PID: %d) finished with status %d\n", pid, WEXITSTATUS(status));
+    }
+
+    return 0;
+}
